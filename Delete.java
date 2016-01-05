b319fd5c823f6633d9e516f61fa2358353351daa
M Waheed Rashad <12003065336>
2016-01-05 23:33:30 +0500

    Search test complete

---
+++ Delete.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author M Waheed Rashad
+ */
+public class BSTTest {
+    
+    public BSTTest() {
+        public BSTTest<Integer> a=null;
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+      a=new BSTTest<Integer>();
+ 	}
+
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void TestDelete() {
+ 	         a.insert(12);
+ 	         a.insert(14);
+                 a.insert(16);
+                 a.insert(18);
+                 assertTrue( a.deleteNode(12));
+                 assertTrue( a.deleteNode(14));
+ 	        
+ 		
+ 	}
