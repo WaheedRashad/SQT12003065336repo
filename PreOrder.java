a5fc037abfcabb99b5ba8894ff7256016ca3a72e
M Waheed Rashad <12003065336>
2016-01-05 23:35:43 +0500

    PreOrder test complete

---
+++ PreOrder.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author M Waheed Rashad
+ */
+public class BSTTest {
+    
+    public BSTTest() {
+        public BSTTest<Integer> a=null;
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+      a=new BSTTest<Integer>();
+ 	}
+
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void TestPreorder() {
+		String c="12,14,16,18";
+ 		String o=a.printpostorder();
+ 		assertNotNull(c.equals(o));
+ 	         
+ 		
+ 	}
